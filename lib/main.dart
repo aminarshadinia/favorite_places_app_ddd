1335befa58b0218c98f31a93b6b96c79f29b832c
arshadinia <aminarshadinia@gmail.com>
2022-03-18 00:42:22 +0330

    feat: add configureInjection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:sample_app/injectable.dart';
 
 void main() {
+  configureInjection(Environment.prod);
   runApp(const MyApp());
 }
 
