ab166fcf4d87027ee0c3cb9f3e7ff69791cfdeed
arshadinia <aminarshadinia@gmail.com>
2022-03-18 00:45:44 +0330

    feat: injectable config file

--- lib/infrastructure/core/firebase_injectable_module.dart
+++ lib/infrastructure/core/firebase_injectable_module.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 
+// this file allow us to integrate with third party pckgs
 @module
 abstract class FirebaseInjectableModule {
   @lazySingleton
