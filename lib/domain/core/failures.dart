ea122f6fa06b449fed10c9cff18c439ffccae09a
arshadinia <aminarshadinia@gmail.com>
2022-03-18 20:09:25 +0330

    feat: add generic type

--- lib/domain/core/failures.dart
+++ lib/domain/core/failures.dart
@@ -5,7 +5,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'failures.freezed.dart';
 
 @freezed
-abstract class ValueFailure<T> with _$ValueFailure {
+abstract class ValueFailure<T> with _$ValueFailure<T> {
   const factory ValueFailure.invalidEmail({
     required String faildValue,
   }) = InvalidEmail<T>;
