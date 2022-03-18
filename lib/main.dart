2e9d23a7e3597b33db65ce624a667e09ec90abda
arshadinia <aminarshadinia@gmail.com>
2022-03-18 21:37:08 +0330

    feat: add firebase initilizeapp to avoid default error

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:injectable/injectable.dart';
 import 'package:sample_app/injectable.dart';
 import 'package:sample_app/presentation/core/app_widget.dart';
 
-void main() {
+void main() async {
   configureInjection(Environment.prod);
+  // WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
   runApp(const AppWidget());
 }
 
