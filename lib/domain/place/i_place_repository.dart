77977776122eca597d0754f85244d10780681bfe
arshadinia <aminarshadinia@gmail.com>
2022-03-29 15:32:43 +0430

    feat: add place functions interface

---
+++ lib/domain/place/i_place_repository.dart
@@ -0,0 +1,13 @@
+
+// interface only holds methods defenitions and not going to hold implementation thats why we call it an interface
+import 'package:dartz/dartz.dart';
+import 'package:sample_app/domain/place/place.dart';
+import 'package:sample_app/domain/place/place_failure.dart';
+
+abstract class IPlaceRepository {
+  Stream<Either<PlaceFailure, Place>> watchAll();
+  Stream<Either<PlaceFailure, Place>> watchUncompleted();
+  Future<Either<PlaceFailure , Unit>> create(Place place);  
+  Future<Either<PlaceFailure , Unit>> update(Place place);  
+  Future<Either<PlaceFailure , Unit>> delete(Place place);  
+}
