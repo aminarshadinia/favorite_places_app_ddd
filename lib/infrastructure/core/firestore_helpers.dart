66271329c9aedf0fdc1fa3308f593407f1a70076
arshadinia <aminarshadinia@gmail.com>
2022-03-29 15:36:40 +0430

    feat: add firestoreX and docRefrenceX in helper widget

---
+++ lib/infrastructure/core/firestore_helpers.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sample_app/injectable.dart';
+import 'package:sample_app/domain/auth/i_auth_facade.dart';
+import 'package:sample_app/domain/core/errors.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+
+extension FirestoreX on FirebaseFirestore {
+  /// The user must be already authenticated when calling this method.
+  /// Otherwise, throws [NotAuthenticatedError].
+  Future<DocumentReference> userDocument() async {
+    final userOption = await getIt<IAuthFacade>().getSignedInUser();
+    final user = userOption.getOrElse(() => throw NotAuthenticatedError());
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.id.getOrCrash());
+  }
+}
+
+extension DocumentReferenceX on DocumentReference {
+  CollectionReference get placeCollection => collection('places');
+
+}
