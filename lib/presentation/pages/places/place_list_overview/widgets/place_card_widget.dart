65ee2762fe44c1540ed61b0085df1aa836326116
arshadinia <aminarshadinia@gmail.com>
2022-04-02 18:07:34 +0430

    feat: add dialog to show details page

--- lib/presentation/pages/places/place_list_overview/widgets/place_card_widget.dart
+++ lib/presentation/pages/places/place_list_overview/widgets/place_card_widget.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sample_app/application/places/place_actor/place_actor_bloc.dart';
-import 'package:sample_app/domain/place/place.dart';
+import 'package:sample_app/presentation/pages/places/place_list_overview/widgets/place_details.dart';
 
 class PlaceCard extends StatelessWidget {
-  // final Place place;
-  // const PlaceCard(this.place, {Key? key}) : super(key: key);
-
   final Map<String, dynamic> place;
   const PlaceCard(this.place, {Key? key}) : super(key: key);
 
@@ -14,16 +11,33 @@ class PlaceCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return PlaceDetals(
+                url: place['image'],
+                title: place['title'],
+                latitude: place['latitude'],
+                longitude: place['longitude'],
+              );
+            },
+          );
+        },
         onLongPress: () {
           final placeActorBloc = context.read<PlaceActorBloc>();
-          // placeActorBloc.add(PlaceActorEvent.deleted(place));
+          // placeActorBloc.add(PlaceActorEvent.deleted(place[placeActorBloc]));
         },
-        leading: Image(
-          image: NetworkImage(place['image']),
+        leading: SizedBox(
           height: 50.0,
           width: 50.0,
+          child: Image(
+            image: NetworkImage(place['image']),
+            height: 50.0,
+            width: 50.0,
+          ),
         ),
-        trailing: const Icon(Icons.favorite_border_outlined),
+        trailing: const Icon(Icons.info_outline),
         title: Text(place['title']),
       ),
       elevation: 10,
@@ -32,3 +46,4 @@ class PlaceCard extends StatelessWidget {
     );
   }
 }
+
